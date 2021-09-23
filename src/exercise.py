11f8b70f7cff57b98f06584344cde590e54d0ac3
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 18:43:54 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    num1 = int(input())
+    num2 = int(input())
+    num3 = int(input())
+    num4 = int(input())
+    answer = average(num1, num2, num3, num4)
+    print("Average: " + str(answer))
+
+def average(num1, num2, num3, num4):
+    total = sum(num1, num2, num3, num4)
+    return total / 4
+
+def sum(num1, num2, num3, num4):
+    return num1 + num2 + num3 + num4
 
 if __name__ == '__main__':
     main()
